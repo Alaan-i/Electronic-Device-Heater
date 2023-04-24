6206a30cb393aa8439bc1e0a25e7982ca2c1d0d1
Alaan <56492620+Alaan-i@users.noreply.github.com>
2023-04-24 19:00:16 -0400

    Code overhaul

--- Heater.ino
+++ Heater.ino
@@ -1,55 +1,32 @@
-int heater = 13; // digital pin 13, built-in LED to indicate state of heater
-int ThermistorPin = 0; // analog pin 0
+int heater = 3; // digital pin 3 on attiny
+float reading, voltage;
+float T;
 
-int Vo;
-float R1 = 1000; // R1 value (10,000k ohms)
-float logR2, R2, T;
-float c1 = 1.009249522e-03, c2 = 2.378405444e-04, c3 = 2.019202697e-07; // constants
-boolean heaterState = false;
-
-int maxTemp = 35, minTemp = 30; // temp range
+int maxTemp = 35, minTemp = 25; // temp range
 
 void setup() {
-   pinMode(heater, OUTPUT); // heater pin 13 init
+   pinMode(heater, OUTPUT); // heater init
    Serial.begin(9600); // baud rate: 9600
 }
-
-void resistorCalculations(){ // thermistor calculations
   
-  Vo = analogRead(ThermistorPin); // analogRead function to read voltage value
-  R2 = R1 * (1023.0 / (float)Vo - 1.0);
-  logR2 = log(R2);
-  T = (1.0 / (c1 + c2*logR2 + c3*logR2*logR2*logR2));
-  T = T - 273.15;
-  
+void tempCalc(){ // thermistor calculations
+  voltage = analogRead(A1); // analogRead function to read voltage value, pin A1
+  float mv = voltage / 1024 * 5 * 1000; //find percentage of input reading
+  T = (mv - 0.5) / 10; //Convert to degrees
 }
 
 void loop() {
   
-  resistorCalculations(); // method call
-  //Serial.println("");
-  Serial.print("\nTemperature: "); // serial messages
-  Serial.print(T); // temp value in celsius
-  Serial.println(" C"); // label
-
-  if (heaterState) { // prints heater state based on boolean state 
-    Serial.println("Heater on and warming up");
-  }
-  if (!heaterState) { // same thing
-    Serial.println("Heater off and cooling down");  
-  }
+  tempCalc(); // method call
   
   delay(2000);
-  
   // if temp too hot, turn heater off
   if ( T >= maxTemp){ // max temp  
     digitalWrite(heater, LOW);  
-    heaterState = false; // heater off  
   } 
   
   // if temp too cold, turn heater on
   else if (T <= minTemp) {// min temp
     digitalWrite(heater, HIGH);
-    heaterState = true; // heater on
   }
 }
